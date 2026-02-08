06ad6f23d067b43a4bade10efb3f94af27f2a783
Amrutha <amruthasirisha88@gmail.com>
2026-02-08 16:03:29 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "suresh:: hello ramesh, how r u?"
+echo "ramesh:: hi suresh, i am fine, thanks for asking. hpw r u doing?"
+echo "suresh:: doing fine. what r u learning?"
+echo "ramesh:: i am learning shell. what about you?"
