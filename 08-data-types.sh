ed3fef0807f6eae0f8428da0e5e223c401611e91
Amrutha <amruthasirisha88@gmail.com>
2026-02-08 17:30:55 +0530

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -11,3 +11,6 @@ echo "Sum is: $SUM"
 FRUITS=("Apple" "Orange" "Kiwi")
 
 echo "fruits are: ${FRUITS[@]}"
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
